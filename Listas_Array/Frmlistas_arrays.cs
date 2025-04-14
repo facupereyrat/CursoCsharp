007fe3c2d68e40f7e1039026c46ff523c95afb51
Facu Pereyra <facupereyrat@outlook.com>
2025-04-14 17:55:13 -0300

    Funcionalidad de botones

--- Listas_Array/Frmlistas_arrays.cs
+++ Listas_Array/Frmlistas_arrays.cs
@@ -52,12 +52,24 @@ namespace CursoCsharp.Listas
         {
             char[] numeros;
             numeros = "0123456789".ToCharArray();
+            panelnumerico.Controls.Clear();
             foreach(char numero in numeros)
             {
                 Button btnnumero = new Button();
                 btnnumero.Text = numero.ToString();
+                btnnumero.Size = new Size(72, 67);
+                btnnumero.FlatStyle = FlatStyle.Flat;
+                btnnumero.ForeColor = Color.FromArgb(192,192,255);
+                btnnumero.FlatAppearance.BorderSize = 3;
                 panelnumerico.Controls.Add(btnnumero);
+                btnnumero.Click += Btnnumero_Click;
             }
+        }
+
+        private void Btnnumero_Click(object sender, EventArgs e)
+        {
+            string numero = ((Button)sender).Text;
+            txtpantalla.Text += numero;
         }
     }
 }
